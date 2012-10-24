d137cb804133ff97f9e98b63d786d737d7d39c55
bewchy <bewchabbacc@gmail.com>
2012-10-24 10:16:49 -0400

    Created index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+		<meta charset="UTF-8" />
+		<title>Stories With Friends</title>
+	</head>
+	<body>
+		<header id="banner">
+			<hgroup>
+				<h1>Stories With Friends</h1>
+				<h2>Where you don't know what's going to happen</h2>
+			</hgroup>
+		</header>
+		<section id="main">
+			<h1>Let the story begin...</h1>
+			<P>Momo is stupid</P>
+		</section>
+		<footer>&copy;Bewchy Productions</footer>
+	</body>
+</html>
